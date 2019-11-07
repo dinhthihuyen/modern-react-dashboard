28884f7e4f2bcd37bb55511f47a853e7a5ef5df7
damminhtien <damminhtienchl@gmail.com>
2019-11-07 16:02:30 +0700

    add new DataTable component

--- src/App.js
+++ src/App.js
@@ -1,33 +1,16 @@
 import React from "react";
 import axios from "axios";
+import DataTable from "containers/Main";
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    console.log('constructor');
-    this.state = {
-      employee: []
-    };
-  }
-
-  componentDidMount() {
-    (async () => {
-      const response = await axios({
-        url: employee_api,
-        method: "get"
-      });
-
-      this.setState({
-        employee: response
-      })
-    })();
-    console.log('componentDidMount');
   }
 
   render() {
     console.log('render');
     return (
-      
+      Hello World;
     );
   }
 }
--- src/config/config_url.js
+++ src/config/config_url.js
@@ -1,1 +1,5 @@
-config_url.js
+const CONFIG_URL = {
+	"list_users_api" = "https://randomuser.me/api/?results=5000"
+}
+
+export default CONFIG_URL;
